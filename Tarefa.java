af091e1ea5f4c4f689ac159d43c2c8dba41775c7
deveju <117952692+deveju@users.noreply.github.com>
2024-04-01 20:33:48 -0300

    Add files via upload

--- Tarefa.java
+++ Tarefa.java
@@ -4,4 +4,18 @@ public class Tarefa {
 	public String estado;
 	public Date data;
 	
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
 }
--- Usuario.java
+++ Usuario.java
@@ -3,4 +3,18 @@ package app;
 public class Usuario {
 	protected String nome;
 	protected boolean admin;
+	
+	public String getNome() {
+		return nome;
+	}
+	public boolean getAdmin() {
+		return admin;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
 }
